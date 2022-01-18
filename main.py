221a1d6573fb91aa59c8eee7c2aa01f6cf67adc0
iskandar0704 <93277798+iskandar0704@users.noreply.github.com>
2022-01-18 12:35:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,18 @@ bot = telebot.TeleBot(config.TOKEN)
 def lalala(message):
 	bot.send_message(message.chat.id,message.text)
 
+GROUP_ID = '+ec8ccBLw6_RkMTFi'
 
+
+
+@bot.message_handler(func=lambda message: message.entities is not None and message.chat.id == GROUP_ID)
+def delete_links(message):
+    for entity in message.entities:  # Пройдёмся по всем entities в поисках ссылок
+        # url - обычная ссылка, text_link - ссылка, скрытая под текстом
+        if entity.type in ["url", "text_link"]: 
+            # Мы можем не проверять chat.id, он проверяется ещё в хэндлере 
+            bot.delete_message(message.chat.id, message.message_id)
+        else:
+            return
+	
 bot.polling(none_stop=True)
